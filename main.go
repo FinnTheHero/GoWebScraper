14da8feb0a125e7da675b0161e9e6b04e625ec69
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-05-04 17:03:57 +0400

    Added arguments and fixed check
    Added arguments for single chapter scraping, multichapter scraping where you can specify from where to where to scrape, or use default values. Fixed check where it would check for either txt or md files and skip the saving if one of them existed, now it checks and saves whichever doesnt exist. General code cleanup.

--- main.go
+++ main.go
@@ -1,16 +1,89 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
 
+var help bool
+var check bool
+var single int
+var multi bool
+var from = 1
+var to = 479
+
+func init() {
+	flag.BoolVar(&help, "help", false, "Show help message")
+	flag.BoolVar(&check, "check", false, "Check if the files exist to not overwrite")
+	flag.IntVar(&single, "single", 0, "Scrape a single chapter instead of all chapters")
+	flag.BoolVar(&multi, "multi", false, "Scrape chapters from x to y")
+}
+
 func main() {
+	flag.Parse()
+
+	if help {
+		fmt.Println("Usage: go run main.go [flags]")
+		flag.PrintDefaults()
+		return
+	}
+
+	// Check if the arguments are provided
+	args := flag.Args()
+	// Check that if arguments are provided they are 2 as needed
+	if multi {
+		if len(args) > 0 && len(args) < 2 {
+			tempFrom, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println("Error converting first argument to integer:", err)
+				return
+			}
+
+			if tempFrom < from || tempFrom > to {
+				fmt.Println("The first argument must be greater than ", from, " and less than ", to)
+				return
+			} else {
+				from = tempFrom
+			}
+		} else if len(args) == 2 {
+			tempFrom, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println("Error converting first argument to integer:", err)
+				return
+			}
+
+			if tempFrom < from || tempFrom > to {
+				fmt.Println("The first argument must be greater than ", from, " and less than ", to)
+				return
+			} else {
+				from = tempFrom
+			}
+
+			tempTo, err := strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println("Error converting second argument to integer:", err)
+				return
+			}
+
+			if tempTo > to || tempTo < from {
+				fmt.Println("The second argument must be less than ", to, " and greater than ", from)
+				return
+			} else {
+				to = tempTo
+			}
+		} else if len(args) > 2 {
+			fmt.Println("Please provide 2 or less arguments for scraping multiple chapters")
+			return
+		}
+	}
+
 	c := colly.NewCollector(colly.AllowedDomains("rln.app", "www.rln.app"))
 
 	baseURL := "https://rln.app/the-beginning-after-the-end-535558/chapter-%d"
@@ -62,46 +135,46 @@ func main() {
 		mdFilename := filepath.Join(mdSubfolder, fmt.Sprintf(baseFileName, fileIndex, "md"))
 
 		// Check for the file existence, specify "check" as a first argument
-		if len(os.Args) > 1 && os.Args[1] == "check" {
-			filenames := []string{txtFilename, mdFilename}
-			for _, filename := range filenames {
+		filenames := []string{txtFilename, mdFilename}
+		for _, filename := range filenames {
+			if check {
 				if _, err := os.Stat(filename); err == nil {
 					fmt.Println("File exists:", filename)
+					continue
 				} else if os.IsNotExist(err) {
 					fmt.Println("File doesn't exist:", filename)
 				} else {
 					fmt.Println("Error finding file:", err)
+					return
 				}
 			}
-			return
-		}
 
-		// Create a file and write the text to it
-		txtFile, txtErr := os.Create(txtFilename)
-		mdFile, mdErr := os.Create(mdFilename)
+			file, err := os.Create(filename)
 
-		// Handle errors of creating the file
-		if txtErr != nil {
-			fmt.Println("Error creating file:", txtErr)
-			return
-		} else if mdErr != nil {
-			fmt.Println("Error creating file:", mdErr)
-			return
-		} else {
-			txtFile.WriteString(finalText)
-			txtFile.Close()
-			fmt.Println("File saved:", txtFilename)
-
-			mdFile.WriteString(finalText)
-			mdFile.Close()
-			fmt.Println("File saved:", mdFilename)
+			if err != nil {
+				fmt.Println("Error creating file:", err)
+				return
+			} else {
+				file.WriteString(finalText)
+				file.Close()
+				fmt.Println("File saved:", filename)
+			}
 		}
 	})
 
-	// Loop over for every chapter
-	for i := 1; i <= 479; i++ {
-		fileIndex = i
-		scrapeURL := fmt.Sprintf(baseURL, i)
+	if single != 0 {
+		// Scrape a single chapter
+		fmt.Println("Scraping chapter:", single)
+		fileIndex = single
+		scrapeURL := fmt.Sprintf(baseURL, single)
 		c.Visit(scrapeURL)
+	} else if single == 0 && multi {
+		// Loop over for every chapter
+		fmt.Println("Scraping chapters from ", from, " to ", to)
+		for i := from; i <= to; i++ {
+			fileIndex = i
+			scrapeURL := fmt.Sprintf(baseURL, i)
+			c.Visit(scrapeURL)
+		}
 	}
 }
